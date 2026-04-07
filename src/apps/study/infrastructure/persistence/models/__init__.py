bfca750c5795f54b2278f8c0b5cb53c9cb047ba7
ruurti <>
2026-04-07 11:40:57 +0700

    feat(study): fix linter

--- src/apps/study/infrastructure/persistence/models/__init__.py
+++ src/apps/study/infrastructure/persistence/models/__init__.py
@@ -1,3 +1,9 @@
 from .study import Study
 from .events import EventDefinition
 from .site import Site, SiteMembership
+
+__all__ = [
+    'Study',
+    'EventDefinition',
+    'Site', 'SiteMembership',
+]
